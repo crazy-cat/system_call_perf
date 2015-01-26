8c49c82be8a859417642ae3b3fc7384cf07052c3
QuShuqian <qsq0519@gmail.com>
2015-01-27 00:36:58 +0800

    add malloc test

--- src/system_call_test.cc
+++ src/system_call_test.cc
@@ -31,6 +31,11 @@ TEST(SystemCallTest, GetTimeOfDay)
 
 // fork
 // 系统对用户创建的进程数限制个数不定 可用ulimit -u 查看
+// sysctl kernel.pid_max
+// 性能：60～8000/s
+// 瓶颈：页表复制 <2.6
+// 并发：单CPU进程切换 30～100W/s
+// K级以上稳定在30W/s
 TEST(SystemCallTest, Fork)
 {
   signal(SIGCHLD, SIG_IGN);
@@ -58,13 +63,33 @@ void * test_thread(void*)
 }
 
 // pthread
-// 正常情况下创建线程的性能比创建进程快一个数量级，每秒10W
+// sysctl kernel.threads-max
+// 性能：5~10W/s 随着并发数的增加而降低，并发1W约5W/s
+// 并发：单CPU线程切换50～150W/s K级以上并发50W/s
+// 非COW情况下创建线程的性能比创建进程快一个数量级，每秒10W
+// 但是在使用COW技术时两者性能相差不大
+// 创建进程主要的开销就是页表的复制
 TEST(SystemCallTest, Pthread)
 {
+  // ulimit -s
+  // pthread_attr_setstacksize
+  // 8K
   pthread_t pid;
   IterFrame(400, pthread_create(&pid, NULL, test_thread, NULL));
 
 //  pthread_join(pid, NULL);
+}
+
+// malloc 1024
+TEST(SystemCallTest, Malloc1k)
+{
+  IterFrame(100*1000, malloc(1024));
+}
+
+// malloc 1024*1024
+TEST(SystemCallTest, Malloc1m)
+{
+  IterFrame(1000, malloc(1024*1024));
 }
 
 // memset 1024
@@ -80,6 +105,22 @@ TEST(SystemCallTest, Memset1m)
 {
   char test_array[1024*1024];
   IterFrame(100*1000, memset(test_array, 0, sizeof(test_array)));
+}
+
+// malloc && memset 1024
+TEST(SystemCallTest, Mallset1k)
+{
+  char* p = NULL;
+  IterFrame(100*1000, p = (char*)malloc(1024);\
+      memset(p, 0, 1024));
+}
+
+// malloc && memset 1024*1024
+TEST(SystemCallTest, Mallset1m)
+{
+  char* p = NULL;
+  IterFrame(10000, p = (char*)malloc(1024*1024);\
+      memset(p, 0, 1024*1024));
 }
 
 // memcpy 1024
